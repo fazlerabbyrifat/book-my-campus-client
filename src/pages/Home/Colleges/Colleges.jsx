b1dceb5ac00ec33e42e7ad3650b7ea4dd44996cb
fazlerabbyrifat <rifatfazlerabby@gmail.com>
2023-07-24 23:00:24 +0600

    colleges data added in home page

--- src/pages/Home/Colleges/Colleges.jsx
+++ src/pages/Home/Colleges/Colleges.jsx
@@ -1,66 +1,72 @@
 import { useEffect, useState } from "react";
+import { FaCalendarAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Colleges = () => {
   const [colleges, setColleges] = useState([]);
 
   useEffect(() => {
-    fetch("/colleges.json")
+    fetch("http://localhost:5000/colleges")
       .then((res) => res.json())
       .then((data) => setColleges(data));
   }, []);
 
   return (
     <div className="bg-gray-800 py-10">
-      <h1 className="text-5xl font-bold uppercase text-center mb-10 text-gray-800">Our Best Institutes</h1>
+      <h1 className="text-5xl font-bold uppercase text-center mb-10 text-white">
+        Our Best Institutes
+      </h1>
       <div className="grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-4">
         {colleges.map((college) => (
-          <div key={college.name} className="rounded-lg shadow-lg overflow-hidden">
+          <div
+            key={college.name}
+            className="rounded-lg shadow-lg overflow-hidden"
+          >
             <div className="relative">
               <img
                 src={college.image}
                 alt=""
-                className="w-full h-56 object-cover object-center transition-transform transform scale-100 hover:scale-105"
+                className="w-full h-56 object-cover object-center transition-opacity hover:opacity-90"
               />
-              <div className="absolute inset-0 bg-black opacity-40 transition-opacity hover:opacity-100"></div>
-              <a
-                href="#"
-                className="absolute bottom-4 left-4 text-white font-semibold text-sm hover:text-blue-300 transition-colors"
-              >
-                Learn More
-              </a>
+              <div className="absolute inset-0 bg-black opacity-40 transition-opacity hover:opacity-0"></div>
             </div>
             <div className="px-6 py-4 text-white">
               <h3 className="text-2xl font-semibold mb-2">{college.name}</h3>
-              <p className="text-gray-600 text-sm h-16 overflow-hidden">{college.description}</p>
-            </div>
-            <div className="py-3 px-6 flex justify-between items-center">
-              <div className="">
-                <span className="mr-1">{college.admissionDates}</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 inline-block align-middle"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5.293 3.293a1 1 0 011.414 0L10 6.586l3.293-3.293a1 1 0 111.414 1.414L11.414 8l3.293 3.293a1 1 0 01-1.414 1.414L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414L8.586 8 5.293 4.707a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
+              <span>
+                Research Works:{" "}
+                {college?.researchWorks?.map((researchWork) => (
+                  <li key={researchWork.title}>{researchWork.title}</li>
+                ))}
+              </span>
+              <div className="flex justify-between items-center gap-3 my-2">
+                <span>
+                  Events:{" "}
+                  {college?.events?.map((event) => (
+                    <li key={event.title}>{event.title}</li>
+                  ))}
+                </span>
+                <span>
+                  Sports:{" "}
+                  {college?.sports?.map((sport) => (
+                    <li key={sport.sport}>{sport.sport}</li>
+                  ))}
+                </span>
               </div>
-              <a
-                href="#"
-                className="text-blue-500 font-semibold text-sm hover:text-blue-700 transition-colors"
-              >
-                More Details
-              </a>
             </div>
+            <div className="py-3 px-6">
+              <p>Admission Dates</p>
+              <div className="flex items-center">
+                <FaCalendarAlt></FaCalendarAlt>
+                <span className="ml-2">{college.admissionDates}</span>
+              </div>
+            </div>
+            <Link>
+              <button className="btn btn-primary ml-5 my-5">See Details</button>
+            </Link>
           </div>
         ))}
       </div>
     </div>
-
   );
 };
 
