f8c1b9084af6293fe70650da41f765c96497551b
fazlerabbyrifat <rifatfazlerabby@gmail.com>
2023-07-24 23:06:21 +0600

    error page is added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,11 +3,13 @@ import Main from "../layout/Main";
 import Home from "../pages/Home/Home/Home";
 import Register from "../pages/Register/Register";
 import Login from "../pages/Login/Login";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
         {
             path: '/',
